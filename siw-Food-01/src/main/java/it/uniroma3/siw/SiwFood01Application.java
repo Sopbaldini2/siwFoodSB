d9b0a7a9aac674350129c30bcd9b121ae596ab88
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-05-14 19:35:45 +0200

    Inizio

---
+++ siw-Food-01/src/main/java/it/uniroma3/siw/SiwFood01Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwFood01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwFood01Application.class, args);
+	}
+
+}
